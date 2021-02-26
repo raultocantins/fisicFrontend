f323502298d62ab77d1d34b4e127f3e252e390c2
raultocantins <alexbraul.ar@gmail.com>
2021-02-26 15:13:23 -0300

    Adding chart to home

---
+++ src/components/charts/ChartAlunos.js
@@ -0,0 +1,99 @@
+import React from "react";
+import Chart from "chart.js";
+import Axios from "axios";
+import CircularProgress from "@material-ui/core/CircularProgress";
+export default class ChartAlunos extends React.Component {
+  state = {
+    quantity: '',
+  };
+
+  componentDidMount() {
+    Axios.get("http://localhost:4000/quantity/alunos")
+      .then((res) => {
+this.setState({
+    quantity:res.data
+})
+
+        var ctx = document.getElementsByClassName("myChart");
+        Chart.defaults.global.defaultFontColor = "black";
+        new Chart(ctx, {
+          type: "bar",
+          data: {
+            labels: [
+              "Janeiro",
+              "Fevereiro",
+              "Março",
+              "Abril",
+              "Maio",
+              "Junho",
+              "Julho",
+              "Agosto",
+              "Setembro",
+              "Outubro",
+              "Novembro",
+              "Dezembro",
+            ],
+            datasets: [
+              {
+                label: "Quantidade de Alunos/Meses",
+                data: this.state.quantity,
+                backgroundColor: [
+                  "rgb(76, 175, 80)",
+                  "rgb(76, 175, 80)",
+                  "rgb(76, 175, 80)",
+                  "rgb(76, 175, 80)",
+                  "rgb(76, 175, 80)",
+                  "rgb(76, 175, 80)",
+                  "rgb(76, 175, 80)",
+                  "rgb(76, 175, 80)",
+                  "rgb(76, 175, 80)",
+                  "rgb(76, 175, 80)",
+                  "rgb(76, 175, 80)",
+                  "rgb(76, 175, 80)",
+                ],
+
+                borderWidth: 1,
+              },
+            ],
+          },
+          options: {
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true,
+                  },
+                },
+              ],
+            },
+          },
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  render() {
+    return (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          overflow: "hidden",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {this.state.quantity ? (
+          <canvas
+            className="myChart"
+            style={{ height: "100%", width: "100%" }}
+          />
+        ) : (
+          <CircularProgress style={{ color: "rgb(76, 175, 80)" }} />
+        )}
+      </div>
+    );
+  }
+}
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import ChartAlunos from '../charts/ChartAlunos'
 import './Home.css'
 
 export default function Home(){
 
     return(
         <div className="home">
-       <h1>Home</h1>
+      <Paper elevation={3} className="graphicBox" >
+ <ChartAlunos/>
+      </Paper>
         </div>
     );
 }
